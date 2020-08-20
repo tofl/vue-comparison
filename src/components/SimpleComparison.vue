897390fb8b6f173308e3643ef9dd671eeba480a9
Tom Flitterman <tfl@pers-ee.com>
2020-08-20 12:47:05 +0200

    Initial commit - packaged the library

---
+++ src/components/SimpleComparison.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    This is a simple comparison component
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SimpleComparison',
+};
+</script>
+
+<style scoped lang="scss">
+</style>
